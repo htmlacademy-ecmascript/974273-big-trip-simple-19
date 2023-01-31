784f457c58b349eec379b209485136eabf6ab8d0
NSergeiV <ttniseva@yandex.ru>
2023-01-31 11:25:18 +0300

    Исправил ошибки найденные наставником при проверке на допуск

--- src/views/sort-view.js
+++ src/views/sort-view.js
@@ -36,7 +36,7 @@ export default class SortView extends RadioGroupView {
    * @param {OptionViewState[]} states
    */
   setOptions(states) {
-    this.innerHTML = states.map((state) => this.createOptionHtml(state)).join('');
+    this.innerHTML = states.map(this.createOptionHtml).join('');
   }
 }
 
